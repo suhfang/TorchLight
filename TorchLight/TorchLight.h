ba9c18165139b9dd83bbbab5ad21b8ed3b0c59a0
suhfangmbeng <suhfangmbeng@gmail.com>
2020-05-02 17:28:00 -0500

    .xcodeproj

---
+++ TorchLight/TorchLight.h
@@ -0,0 +1,19 @@
+//
+//  TorchLight.h
+//  TorchLight
+//
+//  Created by Suh Fangmbeng on 5/2/20.
+//  Copyright © 2020 Hourworth LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TorchLight.
+FOUNDATION_EXPORT double TorchLightVersionNumber;
+
+//! Project version string for TorchLight.
+FOUNDATION_EXPORT const unsigned char TorchLightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TorchLight/PublicHeader.h>
+
+
