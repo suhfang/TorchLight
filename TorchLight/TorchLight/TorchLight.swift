3a1c2a4bd35d3a06a0e34b7b8b3c500f26d265f7
suhfangmbeng <suhfangmbeng@gmail.com>
2020-05-03 15:06:05 -0500

    Initial commit

--- TorchLight/TorchLight/TorchLight.swift
+++ TorchLight/TorchLight/TorchLight.swift
@@ -10,9 +10,19 @@ import Foundation
 import AVFoundation
 
 
+public enum BlinkStyle {
+    public static var (on, off) = (false, false)
+}
+
 public class Torch {
     
     public var captureDevice: AVCaptureDevice?
+    public var timer: Timer?
+    public var blinkStyle: BlinkStyle = .default
+    
+    public enum BlinkStyle {
+        case `default`, trafficate, airplane
+    }
     
     public init() {
         guard let device = AVCaptureDevice.default(for: AVMediaType.video), device.hasTorch, device.hasFlash else {
@@ -56,6 +66,42 @@ public class Torch {
         } else {
             print("Torch Light is already off")
         }
+    }
+    
+    
+    private func trafficate() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {[weak self] (_) in
+            self?.turnOn()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {[weak self] in
+                self?.turnOff()
+            }
+        })
+    }
+    
+    private func startDefaultBlinking() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: {[weak self] (_) in
+            self?.turnOn()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {[weak self] in
+                self?.turnOff()
+            }
+        })
+    }
+    
+    func startBlinking() {
+        switch blinkStyle {
+        case .trafficate: trafficate()
+        case .airplane: airplane()
+        default: startDefaultBlinking()
+        }
+    }
+    
+    private func airplane() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {[weak self] (_) in
+            self?.turnOn()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {[weak self] in
+                self?.turnOff()
+            }
+        })
     }
 
     deinit {
